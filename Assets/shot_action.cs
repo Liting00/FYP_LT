1df782425f56857197e29d86b54ea6baef010363
Yeo Li Ting <c190188@ntu.edu.sg>
2022-03-16 23:55:31 +0800

    random shot point

--- Assets/shot_action.cs
+++ Assets/shot_action.cs
@@ -5,12 +5,16 @@ using UnityEngine.UI;
 
 public class shot_action : MonoBehaviour
 {
-    public Transform bullet_spawnpoint;
+    public Transform[] bullet_spawnpoint;
     public GameObject bullet_prefab;
     public float bullet_speed;
     private bool press;
+    private int count = 0;
+    private int mod;
+    private int random_index;
 
-    public Button yourButton;
+    public Button shotButton;
+    public Button passButton;
 
     void Start()
     {
@@ -18,22 +22,57 @@ public class shot_action : MonoBehaviour
 
     private void Update()
     {
-        if (yourButton.GetComponent<Button>())
+
+        mod = count % 4;
+        Debug.Log("This is the mod value" + mod);
+        if (shotButton.GetComponent<Button>())
         {
-            Button btn = yourButton.GetComponent<Button>();
+            Button btn = shotButton.GetComponent<Button>();
             press = true;
-            btn.onClick.AddListener(TaskOnClick);
+            btn.onClick.AddListener(shotButton_click);
         }
+        else if (passButton.GetComponent<Button>())
+        {
+            Button btn = passButton.GetComponent<Button>();
+            btn.onClick.AddListener(passButton_click);
+        }
+
     }
 
-    void TaskOnClick()
+    void shotButton_click()
     {
         if (press)
         {
-            var bullet = Instantiate(bullet_prefab, bullet_spawnpoint.position, bullet_spawnpoint.rotation);
-            bullet.GetComponent<Rigidbody>().velocity = bullet_spawnpoint.up * bullet_speed;
-            press = false;
-            Debug.Log("button was press");
-       }
+            if (mod == 0)
+            {
+                var bullet = Instantiate(bullet_prefab, bullet_spawnpoint[0].position, bullet_spawnpoint[0].rotation);
+                bullet.GetComponent<Rigidbody>().velocity = bullet_spawnpoint[0].up * bullet_speed;
+                press = false;
+            }
+            else if (mod == 1)
+            {
+                var bullet = Instantiate(bullet_prefab, bullet_spawnpoint[1].position, bullet_spawnpoint[1].rotation);
+                bullet.GetComponent<Rigidbody>().velocity = bullet_spawnpoint[1].up * bullet_speed;
+                press = false;
+            }
+            else if (mod == 2)
+            {
+                var bullet = Instantiate(bullet_prefab, bullet_spawnpoint[2].position, bullet_spawnpoint[2].rotation);
+                bullet.GetComponent<Rigidbody>().velocity = bullet_spawnpoint[2].up * bullet_speed;
+                press = false;
+            }
+            else if (mod == 3)
+            {
+                var bullet = Instantiate(bullet_prefab, bullet_spawnpoint[3].position, bullet_spawnpoint[3].rotation);
+                bullet.GetComponent<Rigidbody>().velocity = bullet_spawnpoint[3].up * bullet_speed;
+                press = false;
+            }
+            count++;
+        }
+    }
+
+    void passButton_click()
+    {
+        count++;
     }
 }
--- Assets/spawning.cs
+++ Assets/spawning.cs
@@ -11,6 +11,7 @@ public class spawning : MonoBehaviour
 
     private float xPos;
     private float zPos;
+    private float time;
 
     private void Start()
     {
@@ -21,16 +22,17 @@ public class spawning : MonoBehaviour
     {
         while (count < 20)
         {
-            xPos = Random.Range(-5f, 5f);
-            zPos = Random.Range(-5f, 5f);
+            xPos = Random.Range(-1f, 1f);
+            zPos = Random.Range(-1f, 1f);
+            time = Random.Range(1f, 10f);
             int npcs_Index = Random.Range(0, npcs.Length);
-            if ((!Physics.CheckSphere(new Vector3(xPos, 0.5f, zPos), 1f, LayerMask.GetMask("npc"))))
+            if ((!Physics.CheckCapsule(new Vector3(xPos, 0.5f, zPos), new Vector3(xPos, 0.5f, zPos), 1f, LayerMask.GetMask("npc"))))
             {
                 Instantiate(npcs[npcs_Index], new Vector3(xPos, 0.5f, zPos), Quaternion.identity);
                 Debug.Log("No Collision");
                 count++;
             }
-            yield return new WaitForSeconds(1f);
+            yield return new WaitForSeconds(time);
             Debug.Log("Collision happened");
         }
     }
