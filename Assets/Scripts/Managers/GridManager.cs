a6081f11aac0dcb98040b9611139a344af1346ea
PickleWeed <62238205+PickleWeed@users.noreply.github.com>
2022-03-19 14:27:27 +0800

    add plane

--- Assets/Scripts/Managers/GridManager.cs
+++ Assets/Scripts/Managers/GridManager.cs
@@ -29,7 +29,7 @@ public class GridManager : MonoBehaviour
         {
             for (int z = 0; z < _height; z++)
             {
-                var spawnedTile = Instantiate(_titlePrefab, new Vector3(x, 0.01f, z), Quaternion.identity);
+                var spawnedTile = Instantiate(_titlePrefab, new Vector3(x, 0.02f, z), Quaternion.identity);
                 spawnedTile.name = $"Tile {x} {z}";
 
                 //even and odd colouring
