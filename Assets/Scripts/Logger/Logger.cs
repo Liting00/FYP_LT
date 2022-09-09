39865171bf4d69ca92d1f66eee4ca85503325b2f
PickleWeed <62238205+PickleWeed@users.noreply.github.com>
2022-09-09 16:09:32 +0800

    Update Logger.cs

--- Assets/Scripts/Logger/Logger.cs
+++ Assets/Scripts/Logger/Logger.cs
@@ -1,6 +1,8 @@
-﻿using Network.Singletons;
+﻿using CsvHelper;
+using Network.Singletons;
 using Newtonsoft.Json;
 using System;
+using System.Data;
 using System.IO;
 using System.Text;
 using Unity.Netcode;
@@ -143,5 +145,39 @@ public class Logger : NetworkSingleton<Logger>
         //    csv.AppendLine(newLine);
         //}
         File.AppendAllText(filePath, csv.ToString());
+    }
+    public static DataTable jsonStringToTable(string jsonContent)
+    {
+        DataTable dt = JsonConvert.DeserializeObject<DataTable>(jsonContent);
+        return dt;
+    }
+    public static string jsonToCSV(string jsonContent, string delimiter)
+    {
+        StringWriter csvString = new StringWriter();
+        using (var csv = new CsvWriter(csvString))
+        {
+            csv.Configuration.SkipEmptyRecords = true;
+            csv.Configuration.WillThrowOnMissingField = false;
+            csv.Configuration.Delimiter = delimiter;
+
+            using (var dt = jsonStringToTable(jsonContent))
+            {
+                foreach (DataColumn column in dt.Columns)
+                {
+                    csv.WriteField(column.ColumnName);
+                }
+                csv.NextRecord();
+
+                foreach (DataRow row in dt.Rows)
+                {
+                    for (var i = 0; i < dt.Columns.Count; i++)
+                    {
+                        csv.WriteField(row[i]);
+                    }
+                    csv.NextRecord();
+                }
+            }
+        }
+        return csvString.ToString();
     }
 }
